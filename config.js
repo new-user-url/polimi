79e1900ba4b230b5fc3405a993c89ee213ff68ef
vladmr617-art <vladmirea30@gmail.com>
2026-02-01 18:56:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "", // Music streaming URL
+        musicUrl: "https://github.com/vladmr617-art/vltn-website/blob/main/Justin%20Bieber%20-%20One%20Time%20(1).mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
